2a0c579410dc17d86f3e0f9070ed710fba2747dc
Biruk-Tesfaye-Legesse-IT <it.biruk.tesfaye2913@gmail.com>
2022-03-25 17:35:07 +0300

    General dashboard layout

---
+++ src/Presentation Layer/components/layout.jsx
@@ -0,0 +1,140 @@
+import { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Navbar from './navbar';
+import { Sidebar } from './sidebar';
+import { Box, Container, Grid } from '@mui/material';
+import { Budget } from '../components/dashboard/budget';
+import { TasksProgress } from '../components/dashboard/tasks-progress';
+import { TotalCustomers } from '../components/dashboard/total-customers';
+import { TotalProfit } from '../components/dashboard/total-profit';
+
+
+const DashboardLayoutRoot = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flex: '1 1 auto',
+  maxWidth: '100%',
+  paddingTop: 64,
+  [theme.breakpoints.up('lg')]: {
+    paddingLeft: 280
+  }
+}));
+
+export const DashboardLayout = (props) => {
+  const { children } = props;
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+
+  return (
+    <>
+      <DashboardLayoutRoot>
+        <Box
+          sx={{
+            display: 'flex',
+            flex: '1 1 auto',
+            flexDirection: 'column',
+            width: '100%'
+          }}
+        >
+            <>
+    <header>
+      <title>
+        Dashboard | Material Kit
+      </title>
+    </header>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth={false}>
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <Budget />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TotalCustomers />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TasksProgress />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TotalProfit sx={{ height: '100%' }} />
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={12}
+            xl={9}
+            xs={12}
+          >
+            {/* <Sales /> */}
+          </Grid>
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xl={3}
+            xs={12}
+          >
+            {/* <TrafficByDevice sx={{ height: '100%' }} /> */}
+          </Grid>
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xl={3}
+            xs={12}
+          >
+            <LatestProducts sx={{ height: '100%' }} />
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={12}
+            xl={9}
+            xs={12}
+          >
+            <LatestOrders />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  </>
+        </Box>
+      </DashboardLayoutRoot>
+      <Navbar onSidebarOpen={() => setSidebarOpen(true)} />
+      <Sidebar
+        onClose={() => setSidebarOpen(false)}
+        open={isSidebarOpen}
+      />
+    </>
+  );
+};
