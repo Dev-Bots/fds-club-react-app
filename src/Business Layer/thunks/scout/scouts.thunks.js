5412e4d0388ba9665b93e9967a3d61a4bf13411f
Yonatan Awoke <it.yonatan.awoke@gmail.com>
2022-03-30 23:58:07 -0700

    created functios for scout thunks

---
+++ src/Business Layer/thunks/scout/scouts.thunks.js
@@ -0,0 +1,38 @@
+import scoutActions from '../../actions/scout/scout.actions'
+
+
+export const getScouts = () => (dispatch) => {
+
+    dispatch(scoutActions.scoutsLoading());
+
+    ScoutDataService.getAllScouts()
+        .then((response) => {
+            console.log('response', response.data);
+            dispatch(scoutActions.scoutsLoaded(response.data))
+        })
+        .catch((error) => dispatch(scoutActions.scoutsLoadingError(error.message)));
+};
+
+export const getScout = (id) => (dispatch) => {
+
+    dispatch(scoutActions.scoutLoading());
+
+    ScoutDataService.getScout(id)
+        .then((response) => {
+            console.log('response', response.data);
+            dispatch(scoutActions.scoutLoaded(response.data))
+        })
+        .catch((error) => dispatch(scoutActions.scoutLoadingError(error.message)));    
+};
+
+export const updateScout = (id, data) => (dispatch) => {
+
+    dispatch(scoutActions.scoutUpdating());
+
+    ScoutDataService.updateScout(id,data)
+        .then((response) => {
+            console.log('response', response.data);
+            dispatch(scoutActions.scoutUpdated(response.data))
+        })
+        .catch((error) => dispatch(scoutActions.scoutUpdateError(error.message)));    
+};
