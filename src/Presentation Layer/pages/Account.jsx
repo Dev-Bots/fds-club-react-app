73ce747854e9ce4f31cffebff51969000ac3828b
Biruk-Tesfaye-Legesse-IT <it.biruk.tesfaye2913@gmail.com>
2022-04-02 22:37:52 +0300

    basic account page design

---
+++ src/Presentation Layer/pages/Account.jsx
@@ -0,0 +1,110 @@
+import { useState } from 'react';
+import { Box, Container, Grid, Typography } from '@mui/material';
+import { AccountProfile } from '../components/account/account-profile';
+import { AccountProfileDetails } from '../components/account/account-profile-details';
+import { styled } from '@mui/system';
+import Navbar from '../components/navbar';
+import { Sidebar } from '../components/sidebar';
+
+// const Account = () => (
+
+// );
+
+// Account.getLayout = (page) => (
+//   <DashboardLayout>
+//     {page}
+//   </DashboardLayout>
+// );
+
+// export default Account;
+
+
+const DashboardLayoutRoot = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flex: '1 1 auto',
+  maxWidth: '100%',
+  paddingTop: 64,
+  [theme.breakpoints.up('lg')]: {
+    paddingLeft: 280
+  }
+}));
+
+export const Account= (props) => {
+  const { children } = props;
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  
+  
+  return (
+    <>
+
+      <DashboardLayoutRoot>
+          
+        <Box
+          sx={{
+            display: 'flex',
+            flex: '1 1 auto',
+            flexDirection: 'column',
+            width: '100%'
+          }}
+        >
+             <>
+    {/* <header>
+      <title>
+        Account 
+      </title>
+    </header> */}
+     <title>
+        Account 
+      </title>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth="lg">
+        <Typography
+          sx={{ mb: 3 }}
+          variant="h4"
+        >
+          Manage Account
+        </Typography>
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xs={12}
+          >
+            <AccountProfile />
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={6}
+            xs={12}
+          >
+            <AccountProfileDetails />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  </>
+        </Box>
+      </DashboardLayoutRoot>
+      <Navbar onSidebarOpen={() => setSidebarOpen(true)} />
+      <Sidebar
+        onClose={() => setSidebarOpen(false)}
+        open={isSidebarOpen}
+      />
+    </>
+  );
+};
+
+
+export default Account;
