67190832bed8fc74bcecd0652cd1470de49137a1
Biruk-Tesfaye-Legesse-IT <it.biruk.tesfaye2913@gmail.com>
2022-04-03 08:36:22 +0300

    auth action types

---
+++ src/Business Layer/actions/auth/auth.action.types.js
@@ -0,0 +1,15 @@
+export default {
+
+    // LOG IN 
+    LOGGING_IN: "LOGGING_IN",
+    LOGGED_IN: "LOGGED_IN",
+    LOG_IN_ERROR: "LOG_IN_ERROR",
+
+    //LOG OUT
+    LOGGING_OUT: "LOGGING_OUT",
+    LOGGED_OUT: "LOGGED_OUT",
+    LOG_OUT_ERROR: "LOG_OUT_ERROR",
+
+    REFRESH_TOKEN: "REFRESH_TOKEN",
+
+}
