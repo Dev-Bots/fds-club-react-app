6e6a5d5a97db5b424d440e16467d8054660bef83
Yonatan Awoke <it.yonatan.awoke@gmail.com>
2022-04-10 14:34:31 -0700

    edited and styled some profile details,bugs remain

--- src/Presentation Layer/components/account/account-profile-details.jsx
+++ src/Presentation Layer/components/account/account-profile-details.jsx
@@ -89,3 +89,110 @@ export const AccountProfileDetails = (props) => {
     console.log('Demo Date: ', values.demoDate);
     console.log(values);
   };
+
+  // const handleDateChange = (newEndDate, newStartDate) => {
+  //   setValues({
+  //     ...values,
+  //     startDate: newStartDate,
+  //     endDate: newEndDate
+  //   });
+  //   console.log(values.startDate);
+  // };
+
+
+  return (
+    <form
+      autoComplete="off"
+      noValidate
+      {...props}
+    >
+      <Card>
+        <CardHeader
+          subheader="The information can be edited"
+          title="Profile"
+        />
+        <Divider />
+        <CardContent>
+          <Grid
+            container
+            spacing={3}
+          >
+            <Grid
+              item
+              md={6}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                helperText="Please specify the first name"
+                label="First name"
+                name="firstName"
+                onChange={handleChange}
+                required
+                value={values.firstName}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              item
+              md={6}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                label="Last name"
+                name="lastName"
+                onChange={handleChange}
+                required
+                value={values.lastName}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              item
+              md={6}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                label="Email Address"
+                name="email"
+                onChange={handleChange}
+                required
+                value={values.email}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              item
+              md={6}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                label="Phone Number"
+                name="phone"
+                onChange={handleChange}
+                // type="number"
+                value={values.phone}
+                variant="outlined"
+                error={values.phone.length > 10}
+                helperText={values.phone.length > 10 ? 'Invalid!' : ' '}
+              />
+            </Grid>
+
+            <Grid
+              item
+              md={6}
+              xs={12}
+            >
+              <TextField
+                fullWidth
+                label="Country"
+                name="country"
+                onChange={handleChange}
+                required
+                value={values.country}
+                variant="outlined"
+              />
+            </Grid>
