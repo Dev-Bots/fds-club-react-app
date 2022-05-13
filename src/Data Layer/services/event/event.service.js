eba8c986dcfe381a3f38ac0dba8e83f928b478e6
Biruk Tesfaye Legesse <it.biruk.tesfaye2913@gmail.com>
2022-05-13 16:25:37 +0300

    event services changed

--- src/Data Layer/services/event/event.service.js
+++ src/Data Layer/services/event/event.service.js
@@ -1,6 +1,7 @@
 import http from "../../helpers/client/api.client";
 
 const EVENT_ENDPOINT = "/api/events/"
+const API_ENDPOINT = "/api/"
 
 class EventDataService {
     getAllEvents = () => http.get(`${EVENT_ENDPOINT}`);
@@ -12,7 +13,7 @@ class EventDataService {
         return http.post(`${EVENT_ENDPOINT}`, data);
     }
     updateEvent(id, data) {
-        return http.put(`${EVENT_ENDPOINT}${id}/`, data);
+        return http.patch(`${EVENT_ENDPOINT}${id}/`, data);
     }
     deleteEvent(id) {
         return http.delete(`${EVENT_ENDPOINT}${id}`);
@@ -22,6 +23,15 @@ class EventDataService {
     }
     findEventByTitle(title) {
         return http.get(`${EVENT_ENDPOINT}?title=${title}`);
+    }
+    buildTeams(eventId) {
+        return http.post(`${API_ENDPOINT}build_teams/, ${eventId}`);
+    }
+    checkFormation(id){
+        return http.get(`${EVENT_ENDPOINT}/check-formation/${id}/`);
+    }
+    closeEvent(id) {
+        return http.post(`${EVENT_ENDPOINT}${id}/close`);
     }
 }
 export default new EventDataService();
