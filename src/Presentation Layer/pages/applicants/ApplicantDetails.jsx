34a7d9b532aaba287d61dc4a8c90df4023c516e7
Tinsae <itistinsae@gmail.com>
2022-06-16 06:59:43 +0300

    applicantDetails.jsx done

---
+++ src/Presentation Layer/pages/applicants/ApplicantDetails.jsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import { Box, Container, Grid, Typography } from '@mui/material';
+import { styled } from '@mui/system';
+import Navbar from '../../components/navbar';
+import { Sidebar } from '../../components/sidebar';
+import ApplicantDetailComponent from '../../components/applicants/applicant-details';
+
+const DashboardLayoutRoot = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flex: '1 1 auto',
+  maxWidth: '100%',
+  paddingTop: 64,
+  [theme.breakpoints.up('lg')]: {
+    paddingLeft: 280
+  }
+}));
+
+const ApplicantDetails= (props) => {
+  // const { children } = props;
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+
+
+  
+  return (
+    <>
+      
+      <DashboardLayoutRoot>
+      
+        <Box
+          sx={{
+            display: 'flex',
+            flex: '1 1 auto',
+            flexDirection: 'column',
+            width: '100%'
+          }}
+        >
+        
+        <>
+
+            <Box
+            component="main"
+            sx={{
+                flexGrow: 1,
+                py: 8
+            }}
+            >
+            <Container maxWidth="lg">
+                <Typography
+                sx={{ mb: 3 }}
+                variant="h4"
+                >
+                Player Details
+                </Typography>
+                <Grid
+                container
+                spacing={3}
+                >
+                
+                <Grid
+                    item
+                    lg={12}
+                    md={12}
+                    xs={12}
+                >
+                    <ApplicantDetailComponent />
+                </Grid>
+                </Grid>
+            </Container>
+            </Box>
+        </>
+                
+            </Box>
+            </DashboardLayoutRoot>
+            <Navbar onSidebarOpen={() => setSidebarOpen(true)} />
+            <Sidebar
+                onClose={() => setSidebarOpen(false)}
+                open={isSidebarOpen}
+            />
+    </>
+  );
+};
+
+
+export default ApplicantDetails;
